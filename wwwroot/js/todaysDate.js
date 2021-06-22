cfa84387e81495fc3cdb1bd6b0d55a61907ba595
Joshua Jainga <jjainga@gmail.com>
2021-06-22 12:55:20 -0700

    finished calendar layout

--- wwwroot/js/todaysDate.js
+++ wwwroot/js/todaysDate.js
@@ -1,17 +1,41 @@
 //setting Today's date and Month
-let today = moment().format('MMMM Do YYYY');
-let month = moment().format('MMMM')
+let today = moment().format('MMMM Do');
+let month = moment().format('MMMM YYYY')
 
 const startOfMonthDay = moment().clone().startOf('month').format('dddd'); //weekday
 const startOfMonth  = moment().clone().startOf('month').format('YYYY-MM'); //current month
 const daysInMonth = moment(startOfMonth, "YYYY-MM").daysInMonth(); //number of days in month
-const startdate = moment().clone().startOf('month').format('Do');
+const startdate = moment().clone().startOf('month').format('D');
+
+const startWeek = moment().startOf('month').week();
+const endWeek = moment().endOf('month').week();
+
+
 
 $('document').ready(function() {
     currentDay();
     currentMonth();
-    calendar(startOfMonthDay)
-})
+    calendarFunction().then((data) => {
+        console.log(data);
+        $('#table tr:last').after(
+            data.map(obj => {
+                return `
+                <tr id='${obj.week}'> 
+                    ${obj.days.map(day => {
+                        return `
+                        <td class='border' value='${day}'>
+                            <a href='https://www.google.com'>
+                                ${day}
+                            </a>
+                        </td>
+                        `
+                    })}
+                </tr>
+                `
+            })
+            )
+        });
+    })
 //creating functions to add to the page
 const currentDay = () => {
     $('#date').text("Today's Date: " + today);
@@ -21,32 +45,13 @@ const currentMonth = () => {
     $('#month').text(month);
 }
 
-const calendar = (day) => {
-    console.log(day);
-    console.log(startdate);
-    switch(day) {
-        case "Sunday":
-            $('td[id="0"][name="sun"]').text(startdate);
-            break;
-        case "Monday":
-            $('td[id="0"][name="mon"]').text(startdate);
-            break;
-        case "Tuesday":
-            console.log("poop");
-            $('td[id="0"][name="tue"]').text(startdate);
-            break;
-        case "Wednesday":
-            $('td[id="0"][name="wed"]').text(startdate);
-            break;
-        case "Thursday":
-            $('td[id="0"][name="thu"]').text(startdate);
-            break;
-        case "Friday":
-            $('td[id="0"][name="fri"]').text(startdate);
-            break;
-        case "Saturday":
-            $('td[id="0"][name="sat"]').text(startdate);
-            break;
+const calendarFunction = async () => {
+    let calendar = []
+    for(var week = startWeek; week<=endWeek; week++){
+        calendar.push({
+          week:week,
+          days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day').format('D'))
+        })
     }
-
+    return await calendar;
 }
