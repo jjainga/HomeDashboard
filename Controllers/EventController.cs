10f978b940a41727d68f481b6f77f7defacd30fd
Joshua Jainga <jjainga@gmail.com>
2021-06-24 06:54:25 -0700

    added event controller

---
+++ Controllers/EventController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeDashboard.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeDashboard.Controllers
+{
+    [Route("api/Event")]
+    [ApiController]
+    public class EventController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public EventController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            return Json(new {data = await _db.Event.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) 
+        {
+            var EventFromDb = await _db.Event.FirstOrDefaultAsync(u => u.Id == id);
+            if(EventFromDb == null) {
+                return Json(new { success = false, message= "Error while Deleting" });
+            }
+            _db.Event.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful"});
+        }
+    }
+}
---
+++ Pages/Calendar/AddEvent.cshtml.cs
@@ -0,0 +1,7 @@
+namespace HomeDashboard.Pages.Calendar
+{
+    public class AddEventModel : PageModel
+    {
+        
+    }
+}
